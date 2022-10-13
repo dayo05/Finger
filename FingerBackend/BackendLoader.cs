11d1fe549859e61dd307907d58321822bff62b23
Dayo <dayo05@daum.net>
2022-10-13 10:14:26 +0900

    Add unit test for loading assembly

--- FingerBackend/BackendLoader.cs
+++ FingerBackend/BackendLoader.cs
@@ -4,6 +4,16 @@ namespace FingerBackend;
 
 public static class BackendLoader
 {
+    private static bool isLoaded = false;
+
     public static IBackend LoadAssembly(string? assembly = null)
-        => Activator.CreateInstance(Assembly.LoadFile(assembly ?? Path.GetFullPath("ext.dll")).GetType("FingerBackend.Backend")) as IBackend;
+    {
+        if (isLoaded) throw new InvalidOperationException("Do not load backend twice.");
+        isLoaded = true;
+        return Activator.CreateInstance(Assembly.LoadFile(assembly ?? Path.GetFullPath("ext.dll"))
+            .GetType("FingerBackend.Backend")) as IBackend;
+    }
+
+    public static IBackend LoadAssemblyRelative(string? assembly = "ext.dll")
+        => LoadAssembly(Path.GetFullPath(assembly));
 }
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -5,11 +5,16 @@ public class Tests
     [SetUp]
     public void Setup()
     {
+        if(!File.Exists("../../../../BackendTest/bin/Debug/net6.0/BackendTest.dll") ||
+           !File.Exists("../../../../ActualBackend/bin/Debug/net6.0/ActualBackend.dll"))
+            Assert.Ignore("Please build whole solution before running unit test.");
     }
 
     [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+    public void LoadAssemblyTest()
+    { 
+        BackendLoader.LoadAssemblyRelative("../../../../BackendTest/bin/Debug/net6.0/BackendTest.dll");
+        Assert.Throws<InvalidOperationException>(() =>
+            BackendLoader.LoadAssemblyRelative("../../../../ActualBackend/bin/Debug/net6.0/ActualBackend.dll"));
     }
 }
