bffe44455b9b2e9126280d2cb1241aa87840408d
hichan0310 <103562171+hichan0310@users.noreply.github.com>
2022-10-20 00:42:10 +0900

    1020 12:27

--- FINGERApp/Form1.cs
+++ FINGERApp/Form1.cs
@@ -26,5 +26,40 @@ namespace FINGERApp
         {
 
         }
+
+        private void recentFolder(object sender, EventArgs e)
+        {
+
+        }
+
+        private void findFolder(object sender, EventArgs e)
+        {
+            //folderBrowserDialog1.ShowDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void dragDropFolder(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            findFolder(sender, e);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("asdf");
+            findFolder(sender, e);
+        }
     }
 }
