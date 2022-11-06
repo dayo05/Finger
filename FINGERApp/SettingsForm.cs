0c072371b79a18ccd90554f97c1a1364b75647e4
dayo05 <dayo05@daum.net>
2022-11-06 19:27:04 +0900

    Add settings form

---
+++ FINGERApp/SettingsForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FINGERApp
+{
+    public partial class SettingsForm : Form
+    {
+        public SettingsForm()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public static class GlobalSettings
+    {
+        public static int MaxTaskCount { get; private set; } = 4;
+        public static bool DecompressDirectory { get; private set; } = false;
+        public static List<string> AnalyzePath { get; private set; } = new List<string>();
+        public static void AddPathToAnalyze(string path)
+        {
+            AnalyzePath.Add(path);
+        }
+        public static void Reanalyze() => Program.Reanalyze();
+    }
+}
